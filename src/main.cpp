a955370102195c79833cf44171240abe1828d78d
Anna <alee531@ucr.edu>
2024-06-29 13:55:58 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,107 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unistd.h>
+#include <sstream>
 
+// Handles echo command
+void Echo(const std::string& input) {
+    if (input.length() == 4) {
+        std::cout << std::endl;
+    } else {
+        std::cout << input.substr(5) << std::endl;
+    }
+    std::cout << "$ ";
+}
+
+bool isexecutable(const char* p) {
+    if (!access(p, X_OK)) {
+        return true;
+    }
+
+    else { 
+        return false;
+    }
+}
+
+// Command is a shell builtin
+bool shellbuiltin(const std::string& command) {
+    return (command == "echo" || command == "type" || command == "exit");
+}
+
+std::vector<std::string> seperateDirectories(const char* p){
+    std::stringstream pathvar(p);
+    std::string split;
+    std::vector<std::string> lst;
+
+    while(std::getline(pathvar, split, ':')) {
+        lst.push_back(split);
+    }
+
+    return lst;
+
+}
+
+std::string findexecutable(const std::string& input) {
+    char* path = getenv("PATH");
+    std::vector<std::string> directories; 
+    std::string filepath;
+    directories = seperateDirectories(path);
+    for (int i = 0; i < directories.size(); ++i) {
+        filepath = directories.at(i) + "/" + input;
+        if (isexecutable(filepath.c_str())) {
+            return filepath;
+        }
+    } 
+
+    return "";
+}
+
+// Handles type command
+void Type(const std::string& input) {
+    std::string command = input.substr(5);
+
+    if (shellbuiltin(command)) {
+        std::cout << command << " is a shell builtin";
+    } 
+    else {
+        std::string path = findexecutable(command);
+        if (!path.empty()) {
+            std::cout << command << " is " << path;
+        } else {
+            std::cout << command << ": not found";
+        }
+    }
+
+    std::cout << std::endl << "$ ";
+}
+
+// Handles unknown commands
+void UnknownCommand(const std::string& input) {
+    std::cout << input << ": command not found\n";
+    std::cout << "$ ";
+}
+
+
+
+// Main function
 int main() {
-  // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-  std::string input;
+    std::cout << "$ ";
+    std::string input;
 
-  while (std::getline(std::cin, input) && input != "exit 0")
-  {
-      
-    if (input.substr(0, 4) == "echo"){
-        if (input.length() == 4) {
-                std::cout << std::endl;
-            } else {
-                std::cout << input.substr(5) << std::endl; 
-            }
-            std::cout << "$ ";
-    }
-    
-    else if (input.find("type") != std::string::npos) {
-    
-        if (input.find("echo", 4) != std::string::npos){
-            std::cout << input.substr(5, input.size() - 1) << " is a shell builtin";
-        }
-        
-        else if (input.find("type", 5) != std::string::npos){
-            std::cout << input.substr(5, input.size() - 1) << " is a shell builtin";
-        }
-        
-        else if (input.find("exit", 5) != std::string::npos){
-            std::cout << input.substr(5, input.size() - 1) << " is a shell builtin";
-        }
-        
-        
-        else if (input.find("cat", 5) != std::string::npos){
-            std::cout << input.substr(5, input.size() - 1) << " is /bin/cat";
-        }
-        
-        else {
-            std::cout << input.substr(5, input.size() - 1) << ": not found";
-        }
-        
-        std::cout << std::endl << "$ ";
-        
-    }
-    
-    else{
-        std::cout << input << ": command not found\n";
-        std::cout << "$ ";
-    }
-    
-    
-    
-  }
 
+    while (std::getline(std::cin, input) && input != "exit 0") {
+        if (input.substr(0, 4) == "echo") {
+            Echo(input);
+        } else if (input.substr(0, 4) == "type") {
+            Type(input);
+        } else {
+            UnknownCommand(input);
+        }
+    }
+
+    return 0;
 }
